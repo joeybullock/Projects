444364dd5f9f1abb11dc7cade9030f5c8e3f2231
Joey Bullock <jbullock@accela.com>
2019-03-05 15:54:53 -0600

    init

---
+++ Dominos/src/Index.js
@@ -0,0 +1,15 @@
+fetch("https://order.dominos.com/power/store-locator?type=Carryout&c=37130&s=", {
+  "credentials": "omit",
+  "headers": {
+    "accept": "application/json, text/javascript, */*; q=0.01",
+    "dpz-language": "en",
+    "dpz-market": "UNITED_STATES",
+    "market": "UNITED_STATES",
+    "x-dpz-d": "ee74516f0378851257bc324deb41ac19a853177e6ee6e0ca59be365bbad1fc2b"
+  },
+  "referrer": "https://order.dominos.com/en/assets/build/xdomain/proxy.html",
+  "referrerPolicy": "no-referrer-when-downgrade",
+  "body": null,
+  "method": "GET",
+  "mode": "cors"
+});
