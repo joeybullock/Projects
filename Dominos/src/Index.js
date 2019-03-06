f661421221ed059a460cf623a186bd1461bd4bea
Joey Bullock <jbullock@accela.com>
2019-03-05 19:56:36 -0600

    init

--- Dominos/src/Index.js
+++ Dominos/src/Index.js
@@ -1,15 +1,17 @@
-fetch("https://order.dominos.com/power/store-locator?type=Carryout&c=37130&s=", {
-  "credentials": "omit",
-  "headers": {
-    "accept": "application/json, text/javascript, */*; q=0.01",
-    "dpz-language": "en",
-    "dpz-market": "UNITED_STATES",
-    "market": "UNITED_STATES",
-    "x-dpz-d": "ee74516f0378851257bc324deb41ac19a853177e6ee6e0ca59be365bbad1fc2b"
-  },
-  "referrer": "https://order.dominos.com/en/assets/build/xdomain/proxy.html",
-  "referrerPolicy": "no-referrer-when-downgrade",
-  "body": null,
-  "method": "GET",
-  "mode": "cors"
-});
+const fetch = require('node-fetch');
+
+async function getStoresNearAddress(zipCode, streetAddress) {
+  const response = await fetch(`https://order.dominos.com/power/store-locator?type=Carryout&c=${zipCode}&s=${streetAddress}`, {
+    headers: {
+      accept: 'application/json, text/javascript, */*; q=0.01',
+    },
+    "referrer": "https://order.dominos.com/en/assets/build/xdomain/proxy.html",
+    "referrerPolicy": "no-referrer-when-downgrade",
+    "body": null,
+    "method": "GET",
+  });
+  const json = await response.json();
+  console.log(json);
+}
+
+getStoresNearAddress('37130', '');
