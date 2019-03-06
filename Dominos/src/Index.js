971d55e1428ab085acb795c6203156e941d49092
Joey Bullock <jbullock@accela.com>
2019-03-05 20:15:42 -0600

    init

--- Dominos/src/Index.js
+++ Dominos/src/Index.js
@@ -1,7 +1,13 @@
 const fetch = require('node-fetch');
+const API_URL = 'https://order.dominos.com/power/store-locator?';
+const orderTypes = {
+  Delivery: 'Delivery',
+  Carryout: 'Carryout',
+};
 
-async function getStoresNearAddress(zipCode, streetAddress) {
-  const response = await fetch(`https://order.dominos.com/power/store-locator?type=Carryout&c=${zipCode}&s=${streetAddress}`, {
+
+async function getStoresNearAddress(orderType, cityStateZip, streetAddress = '') {
+  const response = await fetch(`${API_URL}type=${orderType}&c=${cityStateZip}&s=${streetAddress}`, {
     headers: {
       accept: 'application/json, text/javascript, */*; q=0.01',
     },
@@ -11,7 +17,8 @@ async function getStoresNearAddress(zipCode, streetAddress) {
     "method": "GET",
   });
   const json = await response.json();
-  console.log(json);
+  return (json);
 }
 
-getStoresNearAddress('37130', '');
+getStoresNearAddress('Delivery', 'Murfreesboro, TN 37130', '1903 Cypress Drive')
+  .then(json => console.log(json));
