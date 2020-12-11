78cc0b15e59dec51a238bd4c08ec9715483fccd7
Joey Bullock <jbullock@accela.com>
2020-12-11 00:43:11 -0600

    progress

--- Scrape/Scrape.py
+++ Scrape/Scrape.py
@@ -3,13 +3,28 @@ import requests
 from bs4 import BeautifulSoup as bs
 import re
 
-podlist = []
-r = requests.get("https://anchor.fm/s/a7a0d78/podcast/rss")
-soup = bs(r.content, features="html.parser")
-#summary = soup.find_all("itunes:summary")
-summary = soup.find_all("item" "enclosure")
-for s in summary:
-  string = s.string
-  print(string[3:-5].replace("&nbsp;","").replace("&amp;","&"))
-
-print("-------End-------")
+def html2text(node):
+    if not hasattr(node, 'contents'):
+        return node.replace('&nbsp;', ' ')
+    if node.isSelfClosing:
+        return ' '
+    return ''.join([html2text(x) for x in node.contents])
+def downloadPodcast(n,f):
+  return
+with requests.Session() as req:
+  podnames = []
+  podcasts = []
+  r = requests.get("https://anchor.fm/s/a7a0d78/podcast/rss")
+  xml = bs(r.content, "xml")
+  #summary = soup.find_all("itunes:summary")
+  for item in xml.find_all("item", text=False):
+    for idx, child in enumerate(item.children):
+      if (idx == 13):
+        podcast = (str(child).split("url=\"")[1].split("\"/>")[0])
+      if (idx == 15):
+        filename = (str(child).split("&gt;")[1].split("&lt")[0].replace("&amp;", "&").replace("amp; "," ").replace("nbsp;", " "))
+      downloadPodcast(filename, podcast)
+  res = "\n".join("{} {}".format(x, y) for x, y in zip(podnames, podcasts))
+  print(res)
+    
+  print("-------End-------")
